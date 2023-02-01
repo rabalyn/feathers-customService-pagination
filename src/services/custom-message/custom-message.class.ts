aec8fb0785b4de1a9527afcc67486655e803b984
Nils Mundhenke <nils.mundhenke@tu-darmstadt.de>
2023-02-01 17:11:41 +0100

    Add pagination to find

--- src/services/custom-message/custom-message.class.ts
+++ src/services/custom-message/custom-message.class.ts
@@ -1,5 +1,5 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.class.html#custom-services
-import type { Id, NullableId, Params, ServiceInterface } from '@feathersjs/feathers'
+import type { Id, NullableId, Paginated, Params, ServiceInterface } from '@feathersjs/feathers'
 
 import type { Application } from '../../declarations'
 import type {
@@ -23,8 +23,16 @@ export class CustomMessageService<ServiceParams extends Params = CustomMessagePa
 {
   constructor(public options: CustomMessageServiceOptions) {}
 
-  async find(_params?: ServiceParams): Promise<CustomMessage[]> {
-    return []
+  /*
+  TS Error: https://ts-error-translator.vercel.app/?error=AoJw9gDgpiAuCeACA5AMwJYDsAmzFcQWhQGEBXAZ1jAFsBZKCigQwHMoBlGAN3QGMoAHi4heA4MxDMaFAHx50FRJjCxEzJulaZmAIwA2UQmEIALIyxpGI4aHCQFdGo0SPIJcdM33Ce-KAywpmDYFIIA3vjYAFzKZDS6MADchFAAHrCxVCBYrCkAvgA0iJGw6ZmI2bkFxSJiUB7SFMUesF4+peVZsDmYeYj5skPIAHQAUIiIACrwxMgAFAD6EJJNAPyxdf6NMogAPohkOFAYmFDYAJSIALyyiKC0ikISrFjMZdgRUbGY8YkgKTKGW6vX6g32JW+cQSyVSwMqPWqAwA2gBdeT4JQqNQaChaHQGFwmVwoeYrKQyDaILbiVa7A5HbAnLDnK63e7gGhPL7oGLQ-6AroI0EFCGRXk-P6woEVKp9Apo+TjSaTGZzB5cijPNhvD48vm-GEAuGyxHygZ3A7ig1S40ykFI-KKhRY1TqTTaPSGYyEWZuDXc62So2C+FysFiqGGgUmh3mp3o0YTFWqv0oF668764Mx+3Cx2WyES-nSoXhhWol3KN24-Feom+uZBkt2stmiNWqO20OmkUoyvKlMqtX+nU6PXN6OlsPtgoYxTVnEegne6iNtyT7ux-PxyPFqetmd9hOjIA
+  */
+  async find(_params?: ServiceParams): Promise<Paginated<CustomMessage> | CustomMessage[]> {
+    return {
+      limit: 0,
+      skip: 0,
+      total: 0,
+      data: []
+    }
   }
 
   async get(id: Id, _params?: ServiceParams): Promise<CustomMessage> {
@@ -75,5 +83,8 @@ export class CustomMessageService<ServiceParams extends Params = CustomMessagePa
 }
 
 export const getOptions = (app: Application) => {
-  return { app }
+  return {
+    app,
+    paginate: app.get('paginate')
+  }
 }
