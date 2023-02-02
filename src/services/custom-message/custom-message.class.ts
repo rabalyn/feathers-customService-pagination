b4666cf1a262a2d4fce3422ae15adca18b561eab
Nils Mundhenke <nils.mundhenke@tu-darmstadt.de>
2023-02-02 09:13:25 +0100

    add paginated type to ServiceInterface

--- src/services/custom-message/custom-message.class.ts
+++ src/services/custom-message/custom-message.class.ts
@@ -19,19 +19,17 @@ export interface CustomMessageParams extends Params<CustomMessageQuery> {}
 
 // This is a skeleton for a custom service class. Remove or add the methods you need here
 export class CustomMessageService<ServiceParams extends Params = CustomMessageParams>
-  implements ServiceInterface<CustomMessage, CustomMessageData, ServiceParams, CustomMessagePatch>
+  implements ServiceInterface<Paginated<CustomMessage> | CustomMessage, CustomMessageData, ServiceParams, CustomMessagePatch>
 {
   constructor(public options: CustomMessageServiceOptions) {}
 
-  /*
-  TS Error: https://ts-error-translator.vercel.app/?error=AoJw9gDgpiAuCeACA5AMwJYDsAmzFcQWhQGEBXAZ1jAFsBZKCigQwHMoBlGAN3QGMoAHi4heA4MxDMaFAHx50FRJjCxEzJulaZmAIwA2UQmEIALIyxpGI4aHCQFdGo0SPIJcdM33Ce-KAywpmDYFIIA3vjYAFzKZDS6MADchFAAHrCxVCBYrCkAvgA0iJGw6ZmI2bkFxSJiUB7SFMUesF4+peVZsDmYeYj5skPIAHQAUIiIACrwxMgAFAD6EJJNAPyxdf6NMogAPohkOFAYmFDYAJSIALyyiKC0ikISrFjMZdgRUbGY8YkgKTKGW6vX6g32JW+cQSyVSwMqPWqAwA2gBdeT4JQqNQaChaHQGFwmVwoeYrKQyDaILbiVa7A5HbAnLDnK63e7gGhPL7oGLQ-6AroI0EFCGRXk-P6woEVKp9Apo+TjSaTGZzB5cijPNhvD48vm-GEAuGyxHygZ3A7ig1S40ykFI-KKhRY1TqTTaPSGYyEWZuDXc62So2C+FysFiqGGgUmh3mp3o0YTFWqv0oF668764Mx+3Cx2WyES-nSoXhhWol3KN24-Feom+uZBkt2stmiNWqO20OmkUoyvKlMqtX+nU6PXN6OlsPtgoYxTVnEegne6iNtyT7ux-PxyPFqetmd9hOjIA
-  */
-  async find(_params?: ServiceParams): Promise<Paginated<CustomMessage> | CustomMessage[]> {
+  async find(_params?: ServiceParams) {
+    const data: CustomMessage[] = []
     return {
       limit: 0,
       skip: 0,
       total: 0,
-      data: []
+      data
     }
   }
 
